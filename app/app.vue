34a44a2cf02ec7af380907c00a5bee2b904877e2
Markus Tratschitt <tratschitt.m@gmail.com>
2025-04-24 23:40:43 +0200

    changed app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,20 @@
-<template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+<template lang="pug">
+  v-app
+    NuxtLayout
+    NuxtPage
 </template>
+
+<script lang="ts">
+export default defineComponent({
+  name: 'AppRoot'
+})
+</script>
+
+<style lang="less">
+html, body, #__nuxt {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
+
