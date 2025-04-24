469ed040b63108c019b4a18c80d33be97b00b7e2
Markus Tratschitt <tratschitt.m@gmail.com>
2025-04-24 22:07:31 +0200

    chore: add @nuxtjs/pwa dependency to package.json

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,11 +2,50 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  ssr: false, 
+
+  typescript: {
+    strict: true,
+    typeCheck: true,
+  },
+
+  css: ['@/assets/styles/main.less'], 
+  build: {
+    transpile: ['vuetify'], 
+    
+  },
 
   modules: [
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
-    '@nuxt/test-utils'
-  ]
+    '@nuxt/test-utils',
+    '@nuxt/pwa' 
+  ],
+  
+  //@ts-ignore
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Network Calculator',
+      short_name: 'NetCalc',
+      description: 'A minimalistic tool for network calculations',
+      lang: 'en',
+      display: 'standalone',
+      background_color: '#121212',
+      theme_color: '#1976D2',
+    },
+    meta: {
+      theme_color: '#1976D2',
+      ogSiteName: 'Network Calculator',
+      ogTitle: 'Network Calculator',
+    },
+  },
+
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ['/'], 
+    },
+  }
 })
