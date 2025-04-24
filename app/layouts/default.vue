9a68d3c9c8605522ff83d2f32519351830a75ca8
Markus Tratschitt <tratschitt.m@gmail.com>
2025-04-24 23:43:44 +0200

    added default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  v-app
+    v-main
+      NuxtPage
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'DefaultLayout'
+})
+</script>
