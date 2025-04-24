0669bdd72226ce48ca92b1e05532cf3b162f36d2
Markus Tratschitt <tratschitt.m@gmail.com>
2025-04-24 23:24:52 +0200

    added subnet vue

--- app/pages/subnet.vue
+++ app/pages/subnet.vue
@@ -1,12 +1,59 @@
 <template lang="pug">
-  div
-    
+  CalculatorWrapper(title="Subnet Calculator")
+    v-form(@submit.prevent="calculate")
+      v-row(dense)
+        v-col(cols="12" md="6")
+          InputField(v-model="ip" label="IPv4-Adresse" placeholder="z. B. 192.168.0.1")
+        v-col(cols="12" md="6")
+          InputField(v-model="cidr" label="CIDR" type="number" placeholder="z. B. 24")
+
+      v-btn(color="primary" class="mt-2" type="submit") Berechnen
+
+    ResultBox(:results="resultsToShow")
 </template>
 
 <script lang="ts">
+import { defineComponent, ref, computed } from 'vue'
+import { useSubnetCalculator } from '@/composables/useSubnetCalculator'
+import type { SubnetInfo } from '@/types/network'
 
+export default defineComponent({
+  name: 'SubnetPage',
+  setup() {
+    let ip = ref('')
+    let cidr = ref<number | null>(null)
+    let result = ref<SubnetInfo | null>(null)
+
+    function calculate() {
+      if (!ip.value || cidr.value === null) return
+      let calculator = useSubnetCalculator(ip.value as any, cidr.value)
+      result.value = calculator?.toSubnetInfo() || null
+    }
+
+    let resultsToShow = computed(() => {
+      if (!result.value) return {}
+      let {
+        subnetMask,
+        wildcardMask,
+        networkAddress,
+        broadcastAddress,
+        firstHost,
+        lastHost,
+        numberOfHosts
+      } = result.value
+
+      return {
+        'Subnetzmaske': subnetMask,
+        'Wildcard-Maske': wildcardMask,
+        'Netzwerkadresse': networkAddress,
+        'Broadcast-Adresse': broadcastAddress,
+        'Erster Host': firstHost,
+        'Letzter Host': lastHost,
+        'Anzahl Hosts': numberOfHosts
+      }
+    })
+
+    return { ip, cidr, result, calculate, resultsToShow }
+  }
+})
 </script>
-
-<style lang="less">
-
-</style>
